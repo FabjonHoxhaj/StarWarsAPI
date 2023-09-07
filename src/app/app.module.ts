729f84b6902d0b7459c398a94ff6596548d552dc
unknown <fahox1991@gmail.com>
2023-09-07 22:25:12 +0200

    recoverd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FilmComponent } from './film/film.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
